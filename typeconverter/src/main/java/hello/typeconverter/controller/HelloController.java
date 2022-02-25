f562fbf80df667b615953fe06c064e7049177d80
김평강 <21500185@handong.edu>
2022-02-25 20:14:46 +0900

    스프링 타입 컨버터

---
+++ typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,34 @@
+package hello.typeconverter.controller;
+
+import hello.typeconverter.type.IpPort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request){
+        String data = request.getParameter("data");
+        Integer intValue = Integer.valueOf(data);
+        System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data){
+        System.out.println("data = " + data);
+        return "ok";
+    }
+
+    @GetMapping("/ip-port")
+    public String helloV2(@RequestParam IpPort ipPort){
+        System.out.println("ipPort.getIp() = " + ipPort.getIp());
+        System.out.println("ipPort.getPort() = " + ipPort.getPort());
+        return "ok";
+    }
+}
---
+++ typeconverter/src/test/java/hello/typeconverter/formatter/FormattingConversionServiceTest.java
@@ -0,0 +1,32 @@
+package hello.typeconverter.formatter;
+
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+import org.springframework.format.support.DefaultFormattingConversionService;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class FormattingConversionServiceTest {
+
+    @Test
+    void formattingConversionService(){
+        DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        conversionService.addFormatter(new MyNumberFormatter());
+
+        IpPort ipPort = conversionService.convert("127.0.0.1:8080", IpPort.class);
+        assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1", 8080));
+
+        String convert = conversionService.convert(1000, String.class);
+        assertThat(conversionService.convert("1,000", Long.class)).isEqualTo(1000L);
+
+
+    }
+}
